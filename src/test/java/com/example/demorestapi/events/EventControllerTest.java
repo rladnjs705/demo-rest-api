0d9fa01a66c174eefc91bf34a34f7f1dc633709f
jaewon705 <rladnjs705@naver.com>
2022-09-15 16:42:27 +0900

    Add test description annotation

--- src/test/java/com/example/demorestapi/events/EventControllerTest.java
+++ src/test/java/com/example/demorestapi/events/EventControllerTest.java
@@ -1,14 +1,12 @@
 package com.example.demorestapi.events;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -31,9 +29,9 @@ class EventControllerTest {
     ObjectMapper objectMapper;
 
     @Test
+    @DisplayName("정상적으로 이벤트를 생성하는 테스트")
     void createEvent() throws Exception {
-        Event event = Event.builder()
-                .id(100)
+        EventDto eventDto = EventDto.builder()
                 .name("Spring")
                 .description("REST API Development with Spring")
                 .beginEnrollmentDateTime(LocalDateTime.of(2022,9, 15, 10, 12))
@@ -44,15 +42,12 @@ class EventControllerTest {
                 .maxPrice(200)
                 .limitOfEnrollment(100)
                 .location("강남역 D2")
-                .free(true)
-                .offline(false)
-                .eventStatus(EventStatus.PUBLISHED)
                 .build();
 
         mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaTypes.HAL_JSON)
-                        .content(objectMapper.writeValueAsString(event))
+                        .content(objectMapper.writeValueAsString(eventDto))
                         )
                 .andDo(print())
                 .andExpect(status().isCreated())
@@ -65,6 +60,7 @@ class EventControllerTest {
     }
 
     @Test
+    @DisplayName("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     void createEvent_bad_request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -94,6 +90,7 @@ class EventControllerTest {
     }
 
     @Test
+    @DisplayName("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -105,6 +102,7 @@ class EventControllerTest {
     }
 
     @Test
+    @DisplayName("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
